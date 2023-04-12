1a191661c786ea0b5add63f66fa221123ba95079
mmmrsk <f55651068@gmail.com>
2023-04-12 18:56:13 +0300

    adding steps for OrderClient

--- src/main/java/ru/yandex/practicum/qascooter/orders/OrderClient.java
+++ src/main/java/ru/yandex/practicum/qascooter/orders/OrderClient.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.qascooter.orders;
 
+import io.qameta.allure.Step;
 import io.restassured.response.ValidatableResponse;
 import ru.yandex.practicum.qascooter.couriers.BaseClient;
 import static io.restassured.RestAssured.given;
@@ -8,7 +9,7 @@ public class OrderClient extends BaseClient {
     private static final String ORDER_CREATE = "/api/v1/orders";
     private static final String ORDER_LIST = "/api/v1/orders";
     private static final String ORDER_CANCEL = "/api/v1/orders/cancel";
-
+    @Step("Creating a new order")
     public ValidatableResponse createOrder(Order order) {
         return given()
                 .spec(getSpec())
@@ -17,7 +18,7 @@ public class OrderClient extends BaseClient {
                 .post(ORDER_CREATE)
                 .then();
     }
-
+    @Step("List of orders")
     public ValidatableResponse listOfOrders() {
         return given()
                 .spec(getSpec())
@@ -25,7 +26,7 @@ public class OrderClient extends BaseClient {
                 .get(ORDER_LIST)
                 .then();
     }
-
+    @Step("Cancel order")
     public ValidatableResponse cancelOrder(int track) {
         return given()
                 .spec(getSpec())
