702287057bdee9cd1b3b375a4d19516d85abff10
mmmrsk <f55651068@gmail.com>
2023-04-12 18:54:36 +0300

    adding steps for CourierClient

--- src/main/java/ru/yandex/practicum/qascooter/couriers/CourierClient.java
+++ src/main/java/ru/yandex/practicum/qascooter/couriers/CourierClient.java
@@ -2,13 +2,14 @@ package ru.yandex.practicum.qascooter.couriers;
 
 import io.restassured.response.ValidatableResponse;
 import static io.restassured.RestAssured.given;
+import io.qameta.allure.Step;
 
 public class CourierClient extends BaseClient {
     private static final String COURIER_CREATE = "/api/v1/courier";
     private static final String COURIER_LOGIN = "/api/v1/courier/login";
     private static final String COURIER_DELETE = COURIER_CREATE + "/{courierId}";
 
-
+    @Step("Creating a courier")
     public ValidatableResponse createCourier(Courier courier) {
         return given()
                 .spec(getSpec())
@@ -17,7 +18,7 @@ public class CourierClient extends BaseClient {
                 .post(COURIER_CREATE)
                 .then();
     }
-
+    @Step("Login by courier")
     public ValidatableResponse loginByCourier(CourierCreds courierCreds) {
         return given()
                 .spec(getSpec())
@@ -26,7 +27,7 @@ public class CourierClient extends BaseClient {
                 .post(COURIER_LOGIN)
                 .then();
     }
-
+    @Step("Deleting a courier")
     public ValidatableResponse deleteCourier(int id) {
         return given()
                 .spec(getSpec())
